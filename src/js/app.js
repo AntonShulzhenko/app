53f7566fa1baf7f3c29fcd748df38047888c07f8
Anton Shulzhenko <shulzhenkomailbox@gmail.com>
2016-12-16 21:08:10 +0200

    added routs

--- src/js/app.js
+++ src/js/app.js
@@ -1,11 +1,24 @@
-console.log('Hello world!!!');
-document.getElementById('root').innerHTML = templates['main']();
+(() => {
+  //=require 'routs/*.js'
 
-firebaseConfig = {
-  apiKey: 'AIzaSyDDhvBn7pjXjh0I8o5SuKOhN4s7I_p0wZ8',
-  authDomain: 'insta-294e1.firebaseapp.com',
-  databaseURL: 'https://insta-294e1.firebaseio.com',
-  storageBucket: 'insta-294e1.appspot.com',
-  messagingSenderId: '230889605114'
-};
-firebase.initializeApp(firebaseConfig);
+  // Firebase Initialization
+  const firebaseConfig = {
+    apiKey: 'AIzaSyDDhvBn7pjXjh0I8o5SuKOhN4s7I_p0wZ8',
+    authDomain: 'insta-294e1.firebaseapp.com',
+    databaseURL: 'https://insta-294e1.firebaseio.com',
+    storageBucket: 'insta-294e1.appspot.com',
+    messagingSenderId: '230889605114'
+  };
+  firebase.initializeApp(firebaseConfig);
+
+  const rootElement = document.getElementById('root');
+
+  page('/', main);
+  page('/login', login);
+  page('/signup', signup);
+  page();
+
+  function render(tplName, ctx = {}) {
+    rootElement.innerHTML = templates[tplName](ctx);
+  }
+})();
---
+++ src/js/routs/signup.js
@@ -0,0 +1,3 @@
+function signup() {
+  render('signup');
+}
