7b668bc23f26f5307074182bfb88d38fbebbec3e
Anton Shulzhenko <shulzhenkomailbox@gmail.com>
2016-12-16 19:57:39 +0200

    init firebase

--- src/js/app.js
+++ src/js/app.js
@@ -1,2 +1,11 @@
 console.log('Hello world!!!');
 document.getElementById('root').innerHTML = templates['main']();
+
+firebaseConfig = {
+  apiKey: 'AIzaSyDDhvBn7pjXjh0I8o5SuKOhN4s7I_p0wZ8',
+  authDomain: 'insta-294e1.firebaseapp.com',
+  databaseURL: 'https://insta-294e1.firebaseio.com',
+  storageBucket: 'insta-294e1.appspot.com',
+  messagingSenderId: '230889605114'
+};
+firebase.initializeApp(firebaseConfig);
