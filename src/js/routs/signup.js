8c3766a0255990e3cd4c4314566ceef72fa6942b
Anton Shulzhenko <shulzhenkomailbox@gmail.com>
2016-12-16 21:50:20 +0200

    modified signup.js

--- src/js/routs/signup.js
+++ src/js/routs/signup.js
@@ -1,3 +1,29 @@
 function signup() {
   render('signup');
+
+  let form = document.getElementById('signup-form');
+  let {email, password, passwordConfirm} = form;
+
+  form.addEventListener('submit', submitHandler);
+
+  // 1. Нет обработки серверных ошибок
+  // 2. Нет обработки случая, когда пользователь успешно создан
+  // 3. Нет информативности в ошибках при клиентской валидации
+
+  function submitHandler(event) {
+    let emailValue = email.value;
+    let passwordValue = password.value;
+    let passwordConfirmValue = passwordConfirm.value;
+
+    if((emailValue.indexOf('@') === -1) || (emailValue.indexOf('.') === -1) || passwordValue.length < 6 || (passwordConfirmValue !== passwordValue)) {
+      alert('Error');
+    } else {
+      firebase.auth().createUserWithEmailAndPassword(emailValue, passwordValue)
+        .catch(function(error) {
+          alert(error.message);
+        });
+    }
+
+    event.preventDefault();
+  }
 }
